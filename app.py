e2ceb8b74a4823fde582d710270eeb05f9abcee1
kikuchi-mizuki <kikuchimizuki@kikuchimizukinonotopasokon.local>
2025-06-07 14:25:43 +0900

    fix: StripeManagerのインポートを追加

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ import time
 import google.oauth2.credentials
 import google.auth.transport.requests
 from google.auth.exceptions import RefreshError
+from services.stripe_manager import StripeManager
 
 # 環境変数からclient_secret.jsonを書き出す
 client_secret_json = os.getenv("GOOGLE_CLIENT_SECRET")
